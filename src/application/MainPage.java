b78f36dad0d9fe2dd917c9e9fa0ce0ca26edf4da
Miphix <bcslecgj@gmail.com>
2016-10-21 15:09:29 +0800

    set click type at pane

--- src/application/MainPage.java
+++ src/application/MainPage.java
@@ -1,15 +1,18 @@
 package application;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
 
 public class MainPage {
 	@FXML private ToggleGroup UMLElement;
 	
-	public void test(ActionEvent e) {
-		ToggleButton tb = (ToggleButton) UMLElement.getSelectedToggle();
-		System.out.println(tb.getText());
+	public void test(MouseEvent e) {
+		if (UMLElement.getSelectedToggle() instanceof ToggleButton) {
+			ToggleButton tb = (ToggleButton) UMLElement.getSelectedToggle();
+			System.out.println(tb.getText());
+		}
+		System.out.println(e.getX() + " " + e.getY());
 	}
 }
