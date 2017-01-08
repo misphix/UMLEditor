22f1b14f8eb5d67e965dd19ab6994b62f0683db8
Miphix <bcspcjddof@gmail.com>
2017-01-08 22:52:46 +0800

    Rename some file

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("UmlEditor.fxml"));
             Scene scene = new Scene(root, 800, 600);
             primaryStage.setTitle("UML Editor");
             primaryStage.setScene(scene);
